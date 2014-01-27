1874949affc640883fd2466be96a05c7e54e423d
suns3t <huyhoan@pdx.edu>
2014-01-27 13:49:41 -0800

    Add testcase for users

--- mentor/users/tests.py
+++ mentor/users/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from mentor.users.models import User 
 
 # Create your tests here.
+class UserTest(TestCase):
+	def setUp(self):
+		User.objects.create(first_name="foo", last_name="bar")
+
+	def test_get_full_name(self):
+		foo = User.objects.get(first_name="foo")
+		self.assertEqual(foo.get_full_name(), "foo bar")
+
+	def test_get_short_name(self):
+		foo = User.objects.get(first_name="foo")
+		self.assertEqual(foo.get_short_name(), "f bar")
+
+	def test_has_perm(self):
+		foo = User.objects.get(first_name="foo")
+		self.assertTrue(foo.has_perm("foo"))
+
+	def test_has_module(self):
+		foo = User.objects.get(first_name="foo")
+		self.assertTrue(foo.has_module_perms("foo"))
+
+	def test_is_student(self):
+		foo = User.objects.get(first_name="foo")
+
+		# do it twice to test cache
+		self.assertTrue(foo.is_student)
+		self.assertTrue(foo.is_student)
+
+	def test_is_staff(self):
+		foo = User.objects.get(first_name="foo")
+
+		# do it twice to test cache
+		self.assertFalse(foo.is_staff)
+		self.assertFalse(foo.is_staff)
